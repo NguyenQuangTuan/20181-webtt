a0afd7fa8a182d4192b6605b3e401f77f8c5c14f
NguyenQuangTuan <quangtuan0701@gmail.com>
2018-12-14 03:10:55 +0700

    somw fix

--- client/assets/js/no-concat/firebase-messaging-sw.js
+++ client/assets/js/no-concat/firebase-messaging-sw.js
@@ -14,12 +14,27 @@ messaging.setBackgroundMessageHandler(function (payload) {
     console.log('worker-payload', payload);
     const notification = JSON.parse(payload.notification.body);
     // Customize notification here
-    const notificationTitle = notification.title;
-    const notificationOptions = {
-        body: notification.body,
-        icon: 'https://kipalog.com/assets/static_icon/ktmt-756fbdb880a66654c047992af8c742bd.jpg'
+    let { title } = notification;
+    let notificationOptions = {
+        body: "Bạn có một thông báo mới",
+        icon: '../img/test.jpg'
     };
-    $('#my-header').css('color', 'red');
+    let notificationTitle = "Ting ting"
+    switch (title) {
+        case "POST_CREATED":
+            notificationOptions.body = `${notification.full_name} đã đăng bài viết mới "${notification.title}"`
+            break
+        case "REVIEW_CREATED":
+            if (notification.review) {
+                notificationOptions.body = `${notification.review.full_name} đã review một bài viết của bạn`
+            }
+            break
+        case "SUB_REVIEW_CREATED":
+            if (notification.sub_review) {
+                notificationOptions.body = `${notification.sub_review.full_name} đã trả lời một review của bạn`
+            }
+            break
+    }
 
     return self.registration.showNotification(notificationTitle,
         notificationOptions);
