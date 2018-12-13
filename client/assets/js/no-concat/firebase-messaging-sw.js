dabdfa22e72caa07bca7975cf065d851fb254ee7
NguyenQuangTuan <quangtuan0701@gmail.com>
2018-12-13 19:58:46 +0700

    fix oke

--- client/assets/js/no-concat/firebase-messaging-sw.js
+++ client/assets/js/no-concat/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-simportScripts('https://www.gstatic.com/firebasejs/4.13.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/4.13.0/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/4.13.0/firebase-messaging.js')
 
 /*
