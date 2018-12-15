30e1600652fff90496a283e40b28a5fca9151870
NguyenQuangTuan <quangtuan0701@gmail.com>
2018-12-15 17:18:59 +0700

    fix confix IP

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 8080,
-  api_url: 'http://18.223.166.127:3000',
+  api_url: 'http://18.222.102.95:3000',
   authen: {
     secret: 'test',
     token_expires_in: 60 * 60 * 1000
